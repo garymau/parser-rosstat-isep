14ecc6dd2db02ba706da276e7d9f7849cfb76a0e
=epogrebnyak <e.pogrebnyak@gmail.com>
2017-12-16 03:26:45 +0300

    todo for #8

--- mapper.py
+++ mapper.py
@@ -10,14 +10,15 @@ d1 = dict(
 )
 
 d2 = dict(
-    substring = 'Индекс промышленного производства',
-    column = 3,
-    name = 'INDPRO'
+    substring = 'Продукция сельского хозяйства',
+    name = 'AGROPROD'
 )
 
 
 rows = File(year, month, 'main').from_csv()
 
+
+# TODO: write pseudocode for mapper()
 def mapper(year, month, d):
     pass
 
@@ -25,3 +26,9 @@ assert mapper(year, month, d1) == [
     {'name': 'INDPRO_yoy', 'date': '2017-10', 'value': 100.0},         
     {'name': 'INDPRO_rog', 'date': '2017-10', 'value': 105.7},         
 ]
+
+assert mapper(year, month, d2) == [
+    {'name': 'AGROPROD_bln_rub', 'date': '2017-10', 'value': 733.8},         
+    {'name': 'AGROPROD_yoy', 'date': '2017-10', 'value': 97.5},         
+    {'name': 'AGROPROD_rog', 'date': '2017-10', 'value': 64.7},
+]         
